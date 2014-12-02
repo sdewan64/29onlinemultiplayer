a142b09d7c3e9ffa856f63418ee8669caec3099a
sdewan64 <sdewan64@gmail.com>
2014-12-02 19:00:43 +0600

    Available Game Searching Added
    Available games can be found from account menu.

---
+++ app/src/main/java/com/shaheed/online29/GameListAdapter.java
@@ -0,0 +1,14 @@
+package com.shaheed.online29;
+
+/**
+ * Created by Shaheed on 11/28/2014.
+ */
+public class GameListAdapter {
+    public String host;
+    public String gameId;
+
+    GameListAdapter(String host, String gameId){
+        this.host = host;
+        this.gameId = gameId;
+    }
+}
